858b7cc1d1249f6be0108e259da82be144e592e6
Alexander Wingård <alexander.wingard@gmail.com>
2018-12-09 20:27:15 +0100

    Finish day 1

--- src/aoc_2018/core.clj
+++ src/aoc_2018/core.clj
@@ -1,7 +1,32 @@
 (ns aoc-2018.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
   (:gen-class))
 
+(defn read-input [file]
+  (slurp (io/resource file)))
+
+(defn read-list-of-numbers [s]
+  (->> (str/split s #"\n")
+       (map read-string)))
+
+(defn frequency-calibrator [input]
+  (->> (read-list-of-numbers input)
+       (reduce +)
+       (str)))
+
+(defn find-frequency-calibrator-duplicate [input]
+  (->> (read-list-of-numbers input)
+       (cycle)
+       (reduce (fn [{:keys [seen freq]} num]
+                 (let [new-freq (+ freq num)]
+                   (if (contains? seen new-freq)
+                     (reduced new-freq)
+                     {:seen (conj seen new-freq) :freq new-freq})))
+               {:seen #{0} :freq 0})
+       (str)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Final frequency:" (frequency-calibrator (read-input "input-1.txt")))
+  (println "First frequency duplicate:" (find-frequency-calibrator-duplicate (read-input "input-1.txt"))))
--- test/aoc_2018/core_test.clj
+++ test/aoc_2018/core_test.clj
@@ -1,7 +1,20 @@
 (ns aoc-2018.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [aoc-2018.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn lines [& strings] (str/join "\n" strings))
+
+(deftest frequency-calibrator-test
+  (testing
+    (let [data (lines "+1" "-2" "+3" "+1")]
+      (is (= "3" (frequency-calibrator data))))))
+
+(deftest find-frequency-calibrator-duplicate-test
+  (testing
+      (doseq [{:keys [input expected]} [{:input (lines "+1" "-2" "+3" "+1") :expected "2"}
+                                        {:input (lines "+1" "-1") :expected "0"}
+                                        {:input (lines "+3" "+3" "+4" "-2" "-4") :expected "10"}
+                                        {:input (lines "-6" "+3" "+8" "+5" "-6") :expected "5"}
+                                        {:input (lines "+7" "+7" "-2" "-7" "-4") :expected "14"}]]
+        (is (= expected (find-frequency-calibrator-duplicate input))))))
